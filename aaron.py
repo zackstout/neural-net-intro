e00c0f134f3b77039356b3395cedfb0b3d11da16
Zack Stout <zackstout@gmail.com>
2018-01-23 18:05:04 -0600

    neural net, just using aaron's code as a starting point

---
+++ aaron.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# X = (hours sleeping, hours studying), y = score on test
+
+X = np.array(([2,9],[1,5],[3,6]), dtype=float)
+y = np.array(([92],[86],[89]), dtype=float)
+
+X = X/np.amax(X, axis = 0)
+y = y/100
+
+class Neural_Network(object):
+  def __init__(self):
+    self.inputSize = 2
+    self.outputSize = 1
+    self.hiddenSize = 3
+    self.W1 = np.random.randn(self.inputSize,self.hiddenSize)
+    self.W2 = np.random.randn(self.hiddenSize,self.outputSize)
+  def forward(self, X):
+    self.z = np.dot(X,self.W1)
+    self.z2 = self.sigmoid(self.z)
+    self.z3 = np.dot(self.z2,self.W2)
+    o = self.sigmoid(self.z3)
+    return o
+  def sigmoid(self,s):
+    return 1/(1+np.exp(-s))
+  def sigmoidPrime(self, s):
+    return s * (1 - s)
+
+NN = Neural_Network()
+
+o  = NN.forward(X)
+
+print("Predicted Output: " + str(o))
+print("Actual Output: " + str(y))
